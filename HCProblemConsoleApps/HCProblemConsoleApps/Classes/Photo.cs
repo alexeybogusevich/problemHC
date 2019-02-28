8a0058f31c2293e7ace0783780dbf94e61214c15
Anton <antonsvistunov99@gmail.com>
2019-02-28 21:08:14 +0200

    Inputs added

--- HCProblemConsoleApps/HCProblemConsoleApps/Classes/Photo.cs
+++ HCProblemConsoleApps/HCProblemConsoleApps/Classes/Photo.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace HCProblemConsoleApps.Classes {
-    class Photo {
+namespace HCProblemConsoleApps.Classes
+{
+    class Photo
+    {
         public bool IsHorizontal { get; private set; }
 
         public int Id { get; private set; }
 
         public HashSet<string> Tags { get; private set; }
 
-        public Photo(int id, bool isHorizontal, HashSet<string> tags = null) {
-            if (tags == null) {
+        public Photo(int id, bool isHorizontal, HashSet<string> tags = null)
+        {
+            if (tags == null)
+            {
                 Tags = new HashSet<string>();
-            } else {
+            }
+            else
+            {
                 Tags = tags;
             }
 
@@ -23,30 +28,34 @@ namespace HCProblemConsoleApps.Classes {
             IsHorizontal = isHorizontal;
         }
 
-        public void AddTags(string tag) {
+        public void AddTags(string tag)
+        {
             Tags.Add(tag);
         }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             var sb = new StringBuilder();
 
             sb.Append(IsHorizontal ? "H " : "V ");
             sb.Append(Tags.Count);
 
-            foreach (var tag in Tags) {
+            foreach (var tag in Tags)
+            {
                 sb.Append($" {tag}");
             }
 
             return sb.ToString();
         }
 
-        public int CompareTo(Photo photo2) {
+        public int CompareTo(Photo photo2)
+        {
             int intersection = this.Tags.Intersect(photo2.Tags).Count();
 
             int AwoB = this.Tags.Except(photo2.Tags).Count();
 
             int BwoA = photo2.Tags.Except(this.Tags).Count();
-
+            
             return Math.Min(intersection, Math.Min(AwoB, BwoA));
         }
     }
