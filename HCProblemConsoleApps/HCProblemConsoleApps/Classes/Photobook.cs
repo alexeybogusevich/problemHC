8246ba4fd86b9c0c69a23bed871059df90fc8f5b
Anton <antonsvistunov99@gmail.com>
2019-02-28 20:14:25 +0200

    Photobook class started

--- HCProblemConsoleApps/HCProblemConsoleApps/Classes/Photobook.cs
+++ HCProblemConsoleApps/HCProblemConsoleApps/Classes/Photobook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,40 @@ namespace HCProblemConsoleApps.Classes
 {
     class Photobook
     {
+        private List<Photo> _photos;
+
+        public Photobook()
+        {
+            _photos = new List<Photo>();
+        }
+
+        public Photobook(List<Photo> photos)
+        {
+            _photos = photos;
+        }
+
+        public static Photobook FromFile(string filename)
+        {
+            Photobook result;
+
+            using (var reader = File.OpenText(filename))
+            {
+                var collectionSize = int.Parse(reader.ReadLine());
+
+                var photoList = new List<Photo>();
+
+                string temp;
+
+                while ((temp = reader.ReadLine()) != null)
+                {
+
+                }
+
+                result = new Photobook(photoList);
+
+            }
+
+            return result;
+        }
     }
 }
