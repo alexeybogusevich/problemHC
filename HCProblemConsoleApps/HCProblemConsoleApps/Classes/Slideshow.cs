7f35b75b8b152fa88260eda223a0c9abc4aa0a8a
Denis Kochetkov <deniskochetkov1@gmail.com>
2019-02-28 20:05:30 +0200

    Slideshow slides list and constructot

--- HCProblemConsoleApps/HCProblemConsoleApps/Classes/Slideshow.cs
+++ HCProblemConsoleApps/HCProblemConsoleApps/Classes/Slideshow.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HCProblemConsoleApps.Classes
-{
-    class Slideshow
-    {
+namespace HCProblemConsoleApps.Classes {
+    class Slideshow {
+        public List<Slide> slides;
+
+        Slideshow() {
+            slides = new List<Slide>();
+        }
     }
 }
