c79078c1ae9d847cf0a7ba000f8bb348c8e6ecc8
Denis Kochetkov <deniskochetkov1@gmail.com>
2019-02-28 21:22:20 +0200

    photo to slide. changes in slide

--- HCProblemConsoleApps/HCProblemConsoleApps/Classes/Photo.cs
+++ HCProblemConsoleApps/HCProblemConsoleApps/Classes/Photo.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace HCProblemConsoleApps.Classes
-{
-    class Photo
-    {
+namespace HCProblemConsoleApps.Classes {
+    class Photo {
         public bool IsHorizontal { get; private set; }
 
         public int Id { get; private set; }
 
         public HashSet<string> Tags { get; private set; }
 
-        public Photo(int id, bool isHorizontal, HashSet<string> tags = null)
-        {
+        public Photo(int id, bool isHorizontal, HashSet<string> tags = null) {
             if (tags == null)
             {
                 Tags = new HashSet<string>();
@@ -28,13 +25,11 @@ namespace HCProblemConsoleApps.Classes
             IsHorizontal = isHorizontal;
         }
 
-        public void AddTags(string tag)
-        {
+        public void AddTags(string tag) {
             Tags.Add(tag);
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             var sb = new StringBuilder();
 
             sb.Append(IsHorizontal ? "H " : "V ");
@@ -46,17 +41,6 @@ namespace HCProblemConsoleApps.Classes
             }
 
             return sb.ToString();
-        }
-
-        public int CompareTo(Photo photo2)
-        {
-            int intersection = this.Tags.Intersect(photo2.Tags).Count();
-
-            int AwoB = this.Tags.Except(photo2.Tags).Count();
-
-            int BwoA = photo2.Tags.Except(this.Tags).Count();
-            
-            return Math.Min(intersection, Math.Min(AwoB, BwoA));
         }
     }
 }
--- HCProblemConsoleApps/HCProblemConsoleApps/Classes/Slide.cs
+++ HCProblemConsoleApps/HCProblemConsoleApps/Classes/Slide.cs
@@ -4,28 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HCProblemConsoleApps.Classes
-{
-    class Slide 
-    {
+namespace HCProblemConsoleApps.Classes {
+    class Slide {
         public Photo leftPhoto { get; set; }
         public Photo rightPhoto { get; set; }
 
-        Slide() { }
+        public HashSet<string> Tags { get; private set; }
 
-        Slide(Photo horizontal)
-        {
-            leftPhoto = horizontal;
+        public Slide() {
+            Tags = new HashSet<string>();
         }
 
-        Slide(Photo vertical1, Photo vertical2)
-        {
+        public Slide(Photo horizontal) {
+            leftPhoto = horizontal;
+            Tags = horizontal.Tags;
+        }
+
+        public Slide(Photo vertical1, Photo vertical2) {
             leftPhoto = vertical1;
             rightPhoto = vertical2;
+            Tags = vertical1.Tags;
+            Tags.UnionWith(vertical2.Tags);
         }
 
-        public string GetPhotos()
-        {
+        public string GetPhotos() {
             if (rightPhoto == null)
             {
                 return leftPhoto.Id.ToString();
@@ -34,6 +36,16 @@ namespace HCProblemConsoleApps.Classes
             {
                 return leftPhoto.Id.ToString() + ' ' + rightPhoto.Id.ToString();
             }
+        }
+
+        public int CompareTo(Slide slide2) {
+            int intersection = this.Tags.Intersect(slide2.Tags).Count();
+
+            int AwoB = this.Tags.Except(slide2.Tags).Count();
+
+            int BwoA = slide2.Tags.Except(this.Tags).Count();
+
+            return Math.Min(intersection, Math.Min(AwoB, BwoA));
         }
     }
 }
